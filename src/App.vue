6969cfc31a731d42d0489e2caefce5d508d3536b
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2020-12-23 02:23:35 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
 	<div class="clip-path"></div>
 	<div class="container">
-		<div class="chat-side-bar">
-			<ChatSideBar />
-		</div>
+		<ChatSideBar />
 		<Chat />
 	</div>
 </template>
@@ -11,17 +9,16 @@
 <script>
 import ChatSideBar from "./components/ChatSideBar.vue";
 import Chat from "./components/Chat.vue";
-// import { computed, ref, watch } from "vue";
 
 export default {
 	components: { ChatSideBar, Chat },
 	setup() {},
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 .clip-path {
 	clip-path: polygon(0 0, 100% 0%, 100% 53%, 0 100%);
-	background: #0c121a;
+	background: var(--bg-secondary);
 	height: 70vh;
 	width: 100vw;
 	z-index: -1;
@@ -32,12 +29,5 @@ export default {
 .container {
 	display: flex;
 	flex-direction: row;
-}
-.chat-side-bar {
-	width: 270px;
-	height: 100vh;
-	float: left;
-	background: #191e27;
-	justify-self: flex-start;
 }
 </style>
