eb71105bf5dc95c0a4f61d6bf43538c6ebfa6a7c
Abdelouahed Oumoussa <abdelwahd.oumouss@gmail.com>
2020-12-23 13:51:10 +0100

    Update: chat side bar component

--- src/components/ChatSideBar.vue
+++ src/components/ChatSideBar.vue
@@ -1,69 +1,110 @@
 <template>
-	<div class="side-bar">
-		<div class="chat-info">
-			<div class="top">
-				<span>
-					abdelwahd
-					<span class="status">online</span>
-				</span>
-				<button>New conversation</button>
+	<div class="chat-side-bar">
+		<div class="overlay" @click="hideSideBar"></div>
+		<div class="side-bar">
+			<div role="button" class="burger-menu" @click="hideSideBar"><Arrow /></div>
+			<div class="chat-info">
+				<div class="top">
+					<span>
+						abdelwahd
+						<span @click="showStatus" class="status">
+							online
+							<ul v-if="show" class="status__dropdown">
+								<li class="go-offline">go offline</li>
+								<li class="go-offline">logout</li>
+							</ul>
+						</span>
+					</span>
+					<button class="new-conversation">New conversation</button>
+				</div>
+				<input class="search" type="search" placeholder="Search" />
 			</div>
-			<input type="search" placeholder="Search" />
-		</div>
-		<ul class="conversations">
-			<li v-for="(chat, i) in chats" :key="i" @click="change(chat.id)">
-				<div class="profile-image">
-					<img src="../assets/avatar.svg" alt="Avatar" width="40" />
-				</div>
-				<div class="profile-info">
-					<div class="top-row">
-						<div class="profile-name">
-							{{ chat.name }}
+			<ul class="conversations">
+				<li
+					v-for="(chat, i) in chats"
+					:key="i"
+					@click="change(chat.id)"
+					:class="{ active: chat.id == active }"
+				>
+					<div class="profile-image">
+						<img src="../assets/images/avatar.svg" alt="Avatar" width="40" />
+					</div>
+					<div class="profile-info">
+						<div class="top-row">
+							<div class="profile-name">
+								{{ chat.name }}
+							</div>
+							<div class="timestamp">{{ chat.timestamp }}</div>
 						</div>
-						<div class="timestamp">{{ chat.timestamp }}</div>
+						<div class="bottom-row">
+							<div class="message-preview">{{ chat.text.slice(0, 20) + "..." }}</div>
+						</div>
 					</div>
-					<div class="bottom-row">
-						<div class="message-preview">{{ chat.text.slice(0, 20) + "..." }}</div>
-					</div>
-				</div>
-			</li>
-		</ul>
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
 <script>
 import { useStore } from "vuex";
-import { computed } from "vue";
+import { computed, ref } from "vue";
+import Arrow from "../components/svgs/Arrow.vue";
 
 export default {
+	components: { Arrow },
 	setup() {
 		const store = useStore();
 		let chats = computed(() => store.getters.conversations);
-
+		let active = ref(store.state.currentConversationID);
+		let show = ref(false);
 		const change = id => {
 			store.commit("changeConversation", { id: id });
+			hideSideBar();
+			active.value = id;
+		};
+		const hideSideBar = () => {
+			document.querySelector(".chat-side-bar").setAttribute("data-hide", "true");
+		};
+		const showStatus = () => {
+			if (show.value === true) return (show.value = false);
+			show.value = true;
 		};
 
 		return {
 			chats,
 			change,
+			hideSideBar,
+			active,
+			showStatus,
+			show,
 		};
 	},
 };
 </script>
 
 <style lang="scss" scoped>
+.chat-side-bar {
+	min-width: 300px;
+	height: 100vh;
+	float: left;
+	margin-right: 2px;
+	background: #191e27;
+	justify-self: flex-start;
+	transition: all 0.4s;
+}
 .side-bar {
 	width: 100%;
 	height: 100%;
-	background: #1c232e;
-	backface-visibility: 0;
-	color: white;
+	background: var(--bg-messages-container);
+	.burger-menu {
+		display: none;
+	}
 }
 .chat-info {
 	width: 100%;
 	height: 104px;
-	background: #1b212b;
+	background: var(--bg-header);
 	z-index: 1;
 	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.4);
 	.top {
@@ -86,27 +127,39 @@ export default {
 				width: 6px;
 				height: 6px;
 				left: -10px;
-				bottom: 2px;
+				bottom: 4px;
 				border-radius: 4px;
 				background: rgb(3, 141, 38);
 			}
 			&::after {
 				content: "";
 				position: absolute;
-				right: -6px;
-				bottom: -5px;
+				right: 0px;
+				bottom: -4px;
 				border-top: 8px solid #363b44;
 				border-bottom: 6px solid transparent;
 				border-left: 5px solid transparent;
 				border-right: 5px solid transparent;
 			}
+			&__dropdown {
+				position: absolute;
+				left: -14px;
+				top: 18px;
+				padding: 4px;
+				width: 140%;
+				background: var(--bg);
+				box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.4);
+				text-align: center;
+				li {
+					padding: 4px 0;
+				}
+			}
 		}
-		button {
+		.new-conversation {
 			width: 50%;
 			padding: 10px;
-			color: white;
 			float: right;
-			background: #06e04f73;
+			background: #06722a;
 			border: none;
 			margin: 8px 4px 0 0;
 			border-radius: 8px;
@@ -114,16 +167,16 @@ export default {
 			cursor: pointer;
 		}
 	}
-	input {
+	.search {
 		width: 80%;
 		padding: 10px;
-		background: #252d3a;
+		background: var(--bg-search);
 		border: none;
 		border-radius: 6px;
 		display: block;
 		margin: 3px auto 0 auto;
 		outline: none;
-		color: #f1f1f1;
+		color: var(--color);
 		&::-webkit-search-cancel-button {
 			cursor: pointer;
 		}
@@ -141,10 +194,10 @@ export default {
 		flex-direction: column;
 		cursor: pointer;
 		&:hover {
-			background: #151b24;
+			background: var(--bg-secondary);
 		}
 		&.active {
-			background: #151b24;
+			background: var(--bg-secondary);
 		}
 		.profile-image {
 			width: 20%;
@@ -164,22 +217,69 @@ export default {
 			.top-row {
 				.profile-name {
 					display: inline-block;
-					color: #f1f1f1;
 					width: 70%;
 				}
 				.timestamp {
 					float: right;
 					font-size: 12px;
-					color: #b3b3b3;
 					margin-top: 2px;
+					color: var(--color-lighten);
 				}
 			}
 			.bottom-row {
 				.message-preview {
 					margin-top: 5px;
 					font-size: 14px;
-					color: #cccccc;
+					color: var(--color-lighten);
 				}
+			}
+		}
+	}
+}
+
+@media screen and (max-width: 705px) {
+	.chat-side-bar {
+		position: absolute;
+		transform: translateX(-100%);
+		z-index: 2;
+		box-shadow: 4px 0 6px rgba(0, 0, 0, 0.4);
+	}
+	.chat-side-bar[data-hide="true"] {
+		transform: translateX(-100%);
+		.burger-menu {
+			display: none;
+		}
+	}
+	.chat-side-bar[data-hide="false"] {
+		transform: translateX(0%);
+		.overlay {
+			background-color: rgba(22, 22, 22, 0.5);
+			position: absolute;
+			width: 100vw;
+			height: 100vh;
+			bottom: 0;
+			top: 0;
+			left: 0;
+			right: 0;
+			z-index: -1;
+			transition: all 0.6s;
+		}
+		.burger-menu {
+			display: inline-block;
+		}
+	}
+	.side-bar {
+		position: relative;
+		.burger-menu {
+			position: absolute;
+			cursor: pointer;
+			padding: 0 10px;
+			right: -50px;
+			top: 8px;
+			svg {
+				transform: rotateY(180deg);
+				width: 30px;
+				fill: var(--arrow-color);
 			}
 		}
 	}
